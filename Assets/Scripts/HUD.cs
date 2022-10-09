41124b7d9f362f67cf36f95800b52e025a41cda7
GeneralBearMan <115430703+GeneralBearMan@users.noreply.github.com>
2022-10-09 18:46:04 -0500

    Text display time

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -5,6 +5,7 @@ using TMPro;
 
 public class HUD : MonoBehaviour
 {
+    
     float nextTimeTemperature = 0;
     float nextTimePowerLevel = 0;
     float nextTimeOxygenInterval = 0;
@@ -18,6 +19,8 @@ public class HUD : MonoBehaviour
     public GameObject Power_LevelTxt;
     public GameObject WarningOxygen;
     public GameObject WarningPower;
+    public float oxygenwartime = 0;
+    public float powerwartime = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +31,7 @@ public class HUD : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Time.time >= nextTimeOxygenInterval && oxygen>=0)
+        if(Time.time >= nextTimeOxygenInterval && oxygen>=0+1)
         {
             oxygen--;
             OxygenTxt.GetComponent<TextMeshProUGUI>().SetText("Oxygen: " + oxygen +"%");
@@ -48,20 +51,33 @@ public class HUD : MonoBehaviour
         if (oxygen < 25)
         {
             WarningOxygen.SetActive(true);
+            oxygenwartime = Time.time;
+        }
+        else if (Time.time>=oxygenwartime +5)
+        {
+            WarningOxygen.SetActive(false);
         }
         else WarningOxygen.SetActive(false);
         if (power < 25)
         {
             WarningPower.SetActive(true);
+            powerwartime = Time.time;
+        }
+        else if(Time.time>=powerwartime + 5){
+            WarningPower.SetActive(false);
         }
         else WarningPower.SetActive(false);
         if(oxygen < 10)
         {
+            WarningOxygen.SetActive(true);
             WarningOxygen.GetComponent<TextMeshProUGUI>().SetText("Warning Oxygen levels critical!!!! Return to base");
+            oxygenwartime = Time.time;
         }
         if(power< 10)
         {
+            WarningPower.SetActive(true);
             WarningPower.GetComponent<TextMeshProUGUI>().SetText("Warning Power levels critical!!!! Return to base");
+            powerwartime = Time.time;
         }
     }
     
