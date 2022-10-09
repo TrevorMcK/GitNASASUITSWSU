1a5fc5ee8ef8aa0c591ab97e1fa8adc2fb6a37e2
GeneralBearMan <115430703+GeneralBearMan@users.noreply.github.com>
2022-10-09 18:04:52 -0500

    Added UI
    Power level and temperature functionality

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -5,10 +5,17 @@ using TMPro;
 
 public class HUD : MonoBehaviour
 {
-    float nextTime = 0;
+    float nextTimeTemperature = 0;
+    float nextTimePowerLevel = 0;
+    float nextTimeOxygenInterval = 0;
     float oxygen = 100;
-    public int interval = 1;
+    float power = 100;
+    public int Oxygen_Interval = 1;
+    public int Power_Level_Interval = 5;
+    public int TemperatureInterval = 20;
     public GameObject OxygenTxt;
+    public GameObject TemperatureTxt;
+    public GameObject Power_LevelTxt;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +26,22 @@ public class HUD : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Time.time >= nextTime)
+        if(Time.time >= nextTimeOxygenInterval)
         {
             oxygen--;
             OxygenTxt.GetComponent<TextMeshProUGUI>().SetText("Oxygen: " + oxygen +"%");
-            nextTime += interval;
+            nextTimeOxygenInterval += Oxygen_Interval;
+        }
+        if(Time.time >= nextTimePowerLevel)
+        {
+            power--;
+            Power_LevelTxt.GetComponent<TextMeshProUGUI>().SetText("Power Level: " + power + "%");
+            nextTimePowerLevel += Power_Level_Interval;
+        }
+        if(Time.time >= nextTimeTemperature)
+        {
+            TemperatureTxt.GetComponent<TextMeshProUGUI>().SetText("Temperature: " +Mathf.Round( Random.Range(-272.0f, -250.0f)) + "C");
+            nextTimeTemperature += TemperatureInterval;
         }
     }
 }
