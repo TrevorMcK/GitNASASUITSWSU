610fa08b81d3bb2507a273bba6fc24a2626e9b18
GeneralBearMan <115430703+GeneralBearMan@users.noreply.github.com>
2022-10-09 20:34:34 -0500

    World
    Made the world with rocks!!!!

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
 
 public class HUD : MonoBehaviour
 {
@@ -10,7 +12,7 @@ public class HUD : MonoBehaviour
     float nextTimePowerLevel = 0;
     float nextTimeOxygenInterval = 0;
     float oxygen = 100;
-    float power = 100;
+    public float power = 100;
     public int Oxygen_Interval = 1;
     public int Power_Level_Interval = 1;
     public int TemperatureInterval = 20;
@@ -23,17 +25,20 @@ public class HUD : MonoBehaviour
     public float powerwartime = 0;
     bool warningCalledOx1 = false;
     bool warningCalledOx2 = false;
+    public TextMeshProUGUI oxygencolor;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        oxygencolor = GameObject.Find("OxygenTxt").GetComponent<TextMeshProUGUI>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.time >= nextTimeOxygenInterval && oxygen>=0+1)
+        
+        if (Time.time >= nextTimeOxygenInterval && oxygen>=0+1)
         {
             oxygen--;
             OxygenTxt.GetComponent<TextMeshProUGUI>().SetText("Oxygen: " + oxygen +"%");
@@ -100,6 +105,21 @@ public class HUD : MonoBehaviour
             yield return new WaitForSeconds(delay);
             WarningPower.SetActive(false);
         }
+        if (oxygen >= 75)
+        {
+            oxygencolor.color = new Color(0, 1, 0, 1);
+        }
+        if (oxygen >= 50)
+        {
+            oxygencolor.color = new Color(1, 1, 1, 1);
+        }
+        if (oxygen >= 25)
+        {
+            oxygencolor.color = new Color(1.0f, 0.64f, 0.0f);
+        }
     }
     
+    
+
+
 }
